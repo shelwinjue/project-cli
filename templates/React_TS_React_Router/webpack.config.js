ee45278113d3f5c75a20f1364a280f9c1b60d29e
Shiwen Zhu <zhusw@zhejianglab.org>
2024-10-22 14:30:38 +0800

    feat: 修改css-loader配置

--- templates/React_TS_React_Router/webpack.config.js
+++ templates/React_TS_React_Router/webpack.config.js
@@ -26,7 +26,9 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: {
+                auto: /^((?!node_modules).)*$/,
+              },
             },
           },
           'sass-loader',
